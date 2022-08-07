0b53c818b8b049ee8f2713d5c97324ad6a192fc0
i7N3 <studioo1533@gmail.com>
2022-08-07 14:33:54 +0300

    hackerank: new year chaos

---
+++ tests/hackerank/new-year-chaos/index.ts
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/challenges/new-year-chaos/
+export const solution = (queue: number[]): number | null => {
+  let totalBribes = 0,
+    expectedAtPosition1 = 1,
+    expectedAtPosition2 = 2,
+    expectedAtPosition3 = 3;
+
+  for (let i = 0; i < queue.length; i++) {
+    const current = queue[i];
+
+    if (current === expectedAtPosition1) {
+      expectedAtPosition1 = expectedAtPosition2;
+      expectedAtPosition2 = expectedAtPosition3;
+      expectedAtPosition3++;
+    } else if (current === expectedAtPosition2) {
+      totalBribes += 1;
+      expectedAtPosition2 = expectedAtPosition3;
+      expectedAtPosition3++;
+    } else if (current === expectedAtPosition3) {
+      totalBribes += 2;
+      expectedAtPosition3++;
+    } else {
+      return null; // Too chaotic, more than 3 bribes
+    }
+  }
+
+  return totalBribes;
+};
