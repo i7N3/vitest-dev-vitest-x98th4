4d5cb904a561de88a9bba2a5479524cab3d399af
i7N3 <studioo1533@gmail.com>
2022-09-11 14:20:17 +0400

    hacker-rank: sherlock-and-anagrams

--- tests/hacker-rank/sherlock-and-anagrams/index.test.ts
+++ tests/hacker-rank/sherlock-and-anagrams/index.test.ts
@@ -4,9 +4,9 @@ import { solution } from '.';
 describe('Sherlock and Anagrams', () => {
   it('Sample tests', () => {
     expect(solution('abba')).toBe(4);
-    // expect(solution('abcd')).toBe(0);
-    // expect(solution('ifailuhkqq')).toBe(3);
-    // expect(solution('kkkk')).toBe(10);
-    // expect(solution('cdcd')).toBe(5);
+    expect(solution('abcd')).toBe(0);
+    expect(solution('ifailuhkqq')).toBe(3);
+    expect(solution('kkkk')).toBe(10);
+    expect(solution('cdcd')).toBe(5);
   });
 });
--- tests/hacker-rank/sherlock-and-anagrams/index.ts
+++ tests/hacker-rank/sherlock-and-anagrams/index.ts
@@ -1,24 +1,27 @@
 // https://www.hackerrank.com/challenges/sherlock-and-anagrams/
 export const solution = (s: string): number => {
-  let result = 0;
+  let strs = [];
 
   for (let chunkSize = 1; chunkSize < s.length; chunkSize++) {
-    const parts: { [key: string]: number } = {}; // Map can be used instead
+    let temp = [];
 
     for (let i = 0; i <= s.length - chunkSize; i++) {
-      const key = s
-        .substring(i, i + chunkSize)
-        .split('')
-        .sort()
-        .join('');
-
-      if (parts[key]) parts[key] = parts[key] + 1;
-      else parts[key] = 1;
+      temp.push(s.substr(i, chunkSize).split('').sort().join(''));
     }
 
-    const entries = Object.values(parts);
-    for (let value of entries) if (value > 1) result++;
+    temp = temp.map(matches);
+    strs.push(temp.reduce(sum));
   }
 
-  return result;
+  return strs.reduce(sum);
+};
+
+const sum = (acc: number, next: number) => acc + next;
+
+const matches = (val: string, i: number, arr: string[]) => {
+  let count = 0;
+  for (++i; i < arr.length; i++) {
+    if (val == arr[i]) count++;
+  }
+  return count;
 };
